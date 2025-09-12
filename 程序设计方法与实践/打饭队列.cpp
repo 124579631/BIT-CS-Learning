a40c49f84f7b368d391648d26112acfe0cb24644
124579631 <jianguo_z@163.com>
2025-09-12 19:20:26 +0800

    打饭队列.cpp

---
+++ 程序设计方法与实践/打饭队列.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    queue<string> queue;
+
+    while(n--)
+    {
+        string operation;
+        cin >> operation;
+        if(operation == "Arrive")
+        {
+            string name;
+            cin >> name;
+            queue.push(name);
+        }
+        else if(operation == "Leave")
+        {
+            if(!queue.empty())  
+                queue.pop();
+        }
+        else if(operation == "QueueHead")
+        {
+            if(!queue.empty())
+                cout << queue.front() << endl;
+            else
+                cout << "Empty queue" << endl;
+        }
+        else if(operation == "QueueTail")
+        {
+            if(!queue.empty())
+                cout << queue.back() << endl;
+            else
+                cout << "Empty queue" << endl;
+        }
+    }
+
+    return 0;
+}
